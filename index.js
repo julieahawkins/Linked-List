44541c548283b8247faac36aff53363ded98646b
julie hawkins <julabi@gmail.com>
2017-08-27 11:25:38 -0600

    added jquery, created Bookmark object

--- index.js
+++ index.js
@@ -3,23 +3,26 @@
 //read button event listener >>adds class .read
 //delete button event listener >>deletes
 
-var websiteTitleInput = document.querySelector('.website-title-input');
-var websiteUrlInput = document.querySelector('.website-url-input');
-var enterButton = document.querySelector('.enter-button');
-var bookmark = document.querySelector('article');
-var readButton = document.querySelector('.read-button');
-var deleteButton = document.querySelector('.delete-button');
+var websiteTitleInput = $('.website-title-input');
+var websiteUrlInput = $('.website-url-input');
+var enterButton = $('.enter-button');
+var bookmark = $('article');
+var readButton = $('.read-button');
+var deleteButton = $('.delete-button');
 
 
 
-enterButton.addEventListener('click', addLink);
-readButton.addEventListener('click', toggleRead);
-deleteButton.addEventListener('click', deleteLink);
+enterButton.bind('click', addLink);
+readButton.bind('click', toggleRead);
+deleteButton.bind('click', deleteLink);
 
 
 function addLink() {
-	//  first article title = websiteTitleInput.innerText 
-	//  first article url = websiteUrlInput.innerText 
+	//  title = websiteTitleInput.innerText 
+	//  link = websiteUrlInput.innerText 
+	console.log('hello!');
+	// var bookmark = new Bookmark('title', 'link')
+	// sticks it in the first article
 };
 
 
@@ -32,3 +35,32 @@ function toggleRead() {
 function deleteLink() {
 	// deletes the bookmark
 };
+
+/////////////
+
+function Bookmark(title, link) {
+	this.title = title;
+	this.link = link; 
+}
+
+// Bookmark.prototype.toggleRead =
+
+
+// Bookmark.prototype.deleteLink =
+//make a bookmark(object), give it a title(name)
+// give it a URL(link? function?).
+
+// make new bookmarks(objects) with name and url as arguments
+//figure out how to stick that object in each article
+
+
+//add each new object to the beginning of the array using unshift
+
+//error message for
+
+//object(Bookmark) default unread, .read class toggles 
+
+
+
+
+
